fa869c2e00c19e528cbe93b4f10047922189eda9
maidesu <vorpeti@gmail.com>
2023-02-15 03:20:39 +0100

    Switch to whitespaces

--- resource/Resource.h
+++ resource/Resource.h
@@ -1,25 +1,25 @@
 #pragma once
 
-#define NOTIFY_ID				1
+#define NOTIFY_ID        1
 
-#define IDR_MAINFRAME			100
+#define IDR_MAINFRAME    100
 
-#define IDS_APP_TITLE			101
+#define IDS_APP_TITLE    101
 
-#define IDD_DIALOG				111
-#define IDD_ABOUTBOX			112
-#define IDD_MESSAGE				113
-#define IDD_MENU				114
+#define IDD_DIALOG       111
+#define IDD_ABOUTBOX     112
+#define IDD_MESSAGE      113
+#define IDD_MENU         114
 
-#define IDM_ABOUT				121
-#define IDM_PRIORITY			122
-#define IDM_SUBMIT				123
-#define IDM_EXIT				124
+#define IDM_ABOUT        121
+#define IDM_PRIORITY     122
+#define IDM_SUBMIT       123
+#define IDM_EXIT         124
 
-#define IDI_ICON				131
-#define IDI_SMALL				132
+#define IDI_ICON         131
+#define IDI_SMALL        132
 
-#define IDC_PROJECT				141
+#define IDC_PROJECT      141
 #ifndef IDC_STATIC
-#define IDC_STATIC				-1
+#define IDC_STATIC       -1
 #endif
